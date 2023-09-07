6a03c33200f88991c5c730ab5302802967dc7942
Leonardo Reis <leonardocarlosreis@gmail.com>
2023-09-07 18:11:20 -0300

    Adicionando header e footer base

--- react-app/src/App.jsx
+++ react-app/src/App.jsx
@@ -4,7 +4,17 @@ import AppRouter from './AppRouter';
 function App(props) {
   return (
     <>
+      <header>
+        <h1>Brasa e Queijo Pizzaria</h1>
+      </header>
       <AppRouter/>
+      <footer>
+        <nav>
+          <a href="/">Link1</a>
+          <a href="/">Link2</a>
+          <a href="/">Link3</a>
+        </nav>
+      </footer>
     </>
   );
 }
