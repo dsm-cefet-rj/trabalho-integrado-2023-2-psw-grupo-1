bd899318be27a48fcbc420b83b0155b2cc38a52c
joaoFrc <129303064+joaoFrc@users.noreply.github.com>
2023-09-07 23:43:02 -0300

    Add files via upload

--- react-app/src/telas/pedido.jsx
+++ react-app/src/telas/pedido.jsx
@@ -1,7 +1,24 @@
+import "./pedido.css"
 const TelaPedido = () => {
     return (
       <>
-        <h1>Tela Pedido</h1>
+            <div>
+                <h2 style={{textAlign: 'center'}}>Pedido</h2>
+
+                <div class="retanguloRedondo">
+                    <h3 style={{ textAlign: 'center'}}>2 pizzas calabresa</h3>
+
+
+                    <p>Status do Pedido:</p>
+                    <p>Tempo de Espera:</p>
+                    <p>Local de Entrega:</p>
+                    <p>N° do pedido:</p>
+                </div>
+
+                <div class="retanguloRedondo retanguloContato" style={{ height: 40}}>
+                    <p class="textoCentralizado2">Numero de Contato: 99999-9999</p>
+                </div>
+            </div>
       </>
     );
   }
