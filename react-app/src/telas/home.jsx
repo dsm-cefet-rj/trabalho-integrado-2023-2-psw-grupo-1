f6d0beaeaf23c6bfeac7af27b0a4828b489faf90
Leonardo C. R. Picanço <Leonardocarlosreis@gmail.com>
2023-09-20 19:07:10 -0300

    Bloco de detalhe aparecendo ao clicar no produto

--- react-app/src/telas/home.jsx
+++ react-app/src/telas/home.jsx
@@ -6,19 +6,19 @@ import { changeCarrinho } from "../redux/carrinhoSlice";
 
 const TelaHome = () => {
   const [produtos, setProdutos] = useState(null)
+  const [displayPopUp, setDisplayPopUp] = useState("none")
+  const [isBlur, setIsBlur] = useState(false)
   const dispatch = useDispatch()
   const  carrinho2  = useSelector(state => state.carrinho)
-  
-  const handleClickProduto = (event) => {
+
+  const handleClickConfirmarProduto = (event) => {
     let produtoId = parseInt(event.currentTarget.id);
 
     let produtoAtual = produtos.filter(p => p.id === produtoId)[0]
+
     produtoAtual.idProdutoCarrinho = parseInt(uniqueId())
 
-    console.log(produtoAtual.idProdutoCarrinho)
-
     let valorPizza = produtoAtual.valor
-
     let novoProdutosCarrinho = carrinho2.produtos != null ? [...carrinho2.produtos, produtoAtual] : [produtoAtual]
     let novoValor = carrinho2.valorTotal != null ? parseInt(carrinho2.valorTotal) + parseInt(valorPizza) : valorPizza
     let novaQuantidade = carrinho2.quantidade != null ? carrinho2.quantidade + 1 : 1
@@ -32,7 +32,7 @@ const TelaHome = () => {
 
     dispatch(changeCarrinho(novoCarrinho))
 
-    //O carrinho do usuário vai passar a existir no momento do login, nesse momento se tem como premissa que o carrinho já existe
+    //O carrinho do usuário vai passar a existir no momento do login, nesse momento seem como premissa que o carrinho já existe
     fetch('http://localhost:8000/carrinho/1', {
          "method": "PUT",
          "body": JSON.stringify(carrinho2),
@@ -40,12 +40,14 @@ const TelaHome = () => {
        })
 
     alert("Produto adicionado ao carrinho")
-
-    console.log(carrinho2)
+  }
+  
+  const handleClickProduto = (event) => {
+    setDisplayPopUp("block")
+    setIsBlur(true)
   }
 
     useEffect(() => {
-      console.log("a")
       fetch('http://localhost:8000/produtos')
        .then(response => response.json())
        .then(json => setProdutos(json))
@@ -53,7 +55,20 @@ const TelaHome = () => {
 
   return (
     <>
-      <div className="produtos-container">
+      <div className="pop-pup-pizza-container" style={{display: displayPopUp}}>
+        <div className="pop-up-pizza"> 
+          <div className="pop-up-campos">
+            <h2>Nome Pizza</h2>
+            <p>Descricao: Lorem ipsum dolor sit amet consectetur adipisicing elit. Nesciunt ut reiciendis excepturi praesentium ipsam impedit earum deleniti eligendi, quo, nobis at vel itaque voluptates totam aperiam! Voluptatum necessitatibus illo dolores.</p>
+            <div>Valor: </div>
+            <div className="botoes">
+              <button type="button" onClick={handleClickConfirmarProduto}>Adicionar ao Carrinho</button>
+              <button type="button" >Cancelar</button>
+            </div>
+          </div>
+        </div>
+      </div>
+      <div className={`produtos-container ${isBlur === true && "blur"}`}>
         {produtos && produtos.map(p => {
           return<div key={p.id} className="produto" onClick={handleClickProduto} id={p.id}>
                   <div className="imagem-produto">
