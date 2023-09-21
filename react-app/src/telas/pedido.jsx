b122f20bd6b6b2bc1b1754e5db1c97ef80ffc3dc
Leonardo C. R. Picanço <Leonardocarlosreis@gmail.com>
2023-09-20 22:26:48 -0300

    Mudança nos slices

--- react-app/src/telas/pedido.jsx
+++ react-app/src/telas/pedido.jsx
@@ -1,12 +1,10 @@
+import pedidoSlice from "../redux/pedidoSlice"
 import "./pedido.css"
 import { useSelector } from "react-redux"
 
 const TelaPedido = () => {
     const pagamento = useSelector(state => state.pagamento)
-
-    const handleClick = () => {
-        console.log(pagamento)
-    }
+    const pedido = useSelector(state => state.pedido)
 
     return (
       <>
@@ -15,18 +13,16 @@ const TelaPedido = () => {
 
                 <div class="retanguloRedondo">
                     <h3 style={{ textAlign: 'center'}}>2 pizzas calabresa</h3>
-
-
-                    <p>Status do Pedido:</p>
-                    <p>Tempo de Espera:</p>
-                    <p>Local de Entrega:</p>
-                    <p>Nº do pedido:</p>
+                    <p>Local de entrega: {pedido && pedido.localEntrega}</p>
+                    <p>Status do Pedido: {pedido && pedido.statusPedido}</p>
+                    <p>Tempo de Espera: {pedido && pedido.tempoEntrega} minutos</p>
+                    <p>Local de Entrega: {pedido && pedido.localEntrega}</p>
+                    <p>Nº do pedido: {pedido && pedido.numeroPedido}</p>
                 </div>
 
                 <div class="retanguloRedondo retanguloContato" style={{ height: 40}}>
-                    <p class="textoCentralizado2">Número de Contato: 99999-9999</p>
+                    <p class="textoCentralizado2">Número de Contato: {pedido && pedido.numeroContato}</p>
                 </div>
-                <button onClick={handleClick}>aaa</button>
             </div>
       </>
     );
