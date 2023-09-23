68a66b023cf7371faeba9562b152c2eb058d8582
Leonardo C. R. Picanço <Leonardocarlosreis@gmail.com>
2023-09-23 14:39:34 -0300

    inicio tela de exclusão de produtos

--- react-app/src/AppRouter.jsx
+++ react-app/src/AppRouter.jsx
@@ -4,6 +4,7 @@ import TelaHome from "./telas/home"
 import TelaLogin from "./telas/login"
 import TelaPagamento from "./telas/pagamento";
 import TelaPedido from "./telas/pedido"
+import TelaExcluirProduto from "./telas/excluirProduto";
 import {
     Routes,
     Route
@@ -19,6 +20,7 @@ const AppRouter = () => {
               <Route path="/cadastroProduto" element={<TelaCadastroProduto/>}></Route>
               <Route path="/carrinho" element={<TelaCarrinho/>}></Route>
               <Route path="/pagamento" element={<TelaPagamento/>}></Route>
+              <Route path="/excluirProduto" element={<TelaExcluirProduto/>}></Route>
           </Routes>
       </>
     );
