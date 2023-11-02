1c1889053afcfd3e51175f07df5dd0798c0d5bbd
Leonardo C. R. Picanço <Leonardocarlosreis@gmail.com>
2023-11-02 14:33:15 -0300

    Get Produtos através do back-end

--- back-end/models/produtoSchema.js
+++ back-end/models/produtoSchema.js
@@ -1,5 +1,6 @@
 const mongoose = require('mongoose');
 const Schema = mongoose.Schema;
+const normalize = (...args) => import('normalize-mongoose').then(({default: fetch}) => fetch(...args));
 
 const produtoSchema = new Schema({
     "nome": {
@@ -19,6 +20,8 @@ const produtoSchema = new Schema({
         required: true
     }
 });
+
+produtoSchema.plugin(normalize)
 
 var Produtos = mongoose.model('Produtos', produtoSchema);
 
